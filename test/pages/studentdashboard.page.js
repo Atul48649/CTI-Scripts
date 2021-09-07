99438eb28758da6eeb5a2fc63a18eed7e98c3186
Atul Kumar <atul48649@gmail.com>
2021-09-07 18:57:08 +0530

    Add Student Dashboard and Submitted Worklog page

--- test/pages/studentdashboard.page.js
+++ test/pages/studentdashboard.page.js
@@ -1,7 +1,61 @@
 const Page = require('./page');
 class StudentDashboardPage extends Page{
-    get signOutBtn(){
-        return $('.halfBtn');
+    open(){
+        super.open('http://localhost:3000/student/dashboard');
+    }
+    get ctiLogo(){
+        return $("img.styles_logoImg__3l2qk");
+    }
+    get helloStudentText(){
+        return $('h4*=Hello');
+    }
+    get alertIcon(){
+        return $("img[alt='alert']");
+    }
+    get alertTextMessage(){
+        return $('p*=You have');
+    }
+    get nextWorkshopSchedule(){
+        return $('p*=Your next workshop is scheduled on');
+    }
+    get iWillBeThereBtn(){
+        return $("button=I'll be there");
+    }
+    get conatctUsBtn(){
+        return $("button=Contact us");
+    }
+    get worklogsBtn(){
+        return $('p=Worklogs');
+    }
+    get courseLoginBtn(){
+        return $('p=Course Login');
+    }
+    get reportsBtn(){
+        return $('p=Reports');
+    }
+    get settingsBtn(){
+        return $('p=Settings');
+    }
+    get contactsAndSupervisiorsBtn(){
+        return $('p=Contacts and Supervisors');
+    }
+    get activitiesBtn(){
+        return $('p=Activities');
+    }
+    get reachedKnowledgeLevelText(){
+        return $('p=You have reached knowledge level ');
+    }
+    get trophyIcon(){
+        return $("img[src*='trophyImg']");
+    }
+    get signoutBtn(){
+        return $("button=Signout");
+    }
+    get timeCompleted(){
+        return $('p=Time Completed');
+    }
+    get unitsCompleted(){
+        return $('p=Units Completed');
     }
 }
 
---
+++ test/pages/submittedworklog.page.js
@@ -0,0 +1,30 @@
+const Page = require('./page');
+class SubmittedWorklogPage extends Page{
+    open(){
+        // TODO:url needs to be added
+        super.open('');
+    }
+    get backIcon(){
+        return $('img..styles_leftImg__2I6RD');
+    }
+    get ctiLogo(){
+        return $('src*=/static/media/logo.6fe1eebd');
+    }
+    get activitiesText(){
+        return $('div=Activities');
+    }
+    get submittedWorklogsText(){
+        return $('div=Submitted Worklogs');
+    }
+    get editIcon(){
+        return $('');
+    }
+    get moveForwardIcon(){
+        return $('');
+    }
+    get createNewWorklogBtn(){
+        return $('button=Create New Work Log');
+    }
+}
+
+module.exports = new SubmittedWorklogPage();
